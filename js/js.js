3e809880efdad8e6c5bcb6cdfcd4a61862be00ce
xiuchundu <1541131845@qq.com>
2017-11-07 00:02:21 +0800

    :fire:38th commit

--- js/js.js
+++ js/js.js
@@ -176,3 +176,16 @@ function checkEmail(){
 　　　　alert("邮箱格式不对!");
     }
 }
+
+
+
+
+    <script type="text/javascript">
+    $(function(){
+    $(".flip").click(function(){
+        $(".panel").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
+    </script>
+
