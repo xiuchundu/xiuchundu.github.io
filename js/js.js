553b9629c2dbc16e7b36e88cfdd23e7b90727a26
xiuchundu <1541131845@qq.com>
2017-11-07 00:02:55 +0800

    :fire:38th commit

--- js/js.js
+++ js/js.js
@@ -180,12 +180,10 @@ function checkEmail(){
 
 
 
-    <script type="text/javascript">
     $(function(){
     $(".flip").click(function(){
         $(".panel").slideToggle("slow");
         $(".xs1").toggle();
         $(".xs2").toggle();
       });});
-    </script>
 
