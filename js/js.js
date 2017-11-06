5ab5232c0303e214bd7aad755ab7837d0ee90026
xiuchundu <1541131845@qq.com>
2017-11-07 00:05:05 +0800

    :fire:38th commit

--- js/js.js
+++ js/js.js
@@ -178,10 +178,10 @@ function checkEmail(){
 }
 
 
-
+function zhankai(){
     $(".flip").click(function(){
         $(".panel").slideToggle("slow");
         $(".xs1").toggle();
         $(".xs2").toggle();
       })
-
+}
